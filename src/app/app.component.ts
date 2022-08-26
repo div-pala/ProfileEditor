a3f7cf6f7189b5561815621b41e6950734102344
Divya Palaparthy <divya.palaparthy@gmail.com>
2022-08-25 21:00:31 -0500

    Initial commit as per the assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProfileEditor';
+  title = 'Profile Editor';
+  pageTitle:string = "User Information" 
+
+  user:User = {
+    name : 'Divya',
+    age : 10,
+    favoriteColor : 'Orange'
+  }
+
+  formUser:User = {
+    name : '',
+    age : 0,
+    favoriteColor : ''
+  }
+  displayEdit:boolean = false;
+
+  toggleEdit():void {
+    if(this.displayEdit == true){
+      this.displayEdit=false;
+    } else{
+      this.pageTitle = "Update User Information";
+      this.displayEdit=true;
+    }
+    //this.displayEdit == true? this.displayEdit=false : this.displayEdit=true;
+  }
+
+  saveChanges(form:NgForm):void {
+    this.formUser.name = this.user.name;
+    this.formUser.age = this.user.age;
+    this.formUser.favoriteColor = this.user.favoriteColor;
+  }
+
 }
